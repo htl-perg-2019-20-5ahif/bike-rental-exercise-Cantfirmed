37c19315ba2645a9eace465d881698c8bc03cc45
Patrick Wegerer <patrick.wegerer@gmail.com>
2019-11-20 16:05:46 +0100

    Finished most of the requirements for now

---
+++ BikeRental/Controllers/BikesController.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using BikeRental;
+using BikeRental.Model;
+
+namespace BikeRental.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BikesController : ControllerBase
+    {
+        private readonly BikeRentalContext _context;
+
+        public BikesController(BikeRentalContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Bikes
+        // Only active Bikes!
+        [HttpGet]
+        [Route("{sortedBy?}")]
+        public async Task<ActionResult<IEnumerable<Bike>>> GetBikes(string sortedBy)
+        {
+            
+            switch (sortedBy)
+            {
+                case "PriceOfFirstHour": 
+                    return await _context.Bikes
+                        .Where(b => _context.Rentals.Find(b.BikeId) == null || 
+                                _context.Rentals.Find(b.BikeId).Paid || 
+                                _context.Rentals.Find(b.BikeId).TotalCost == 0)
+                        .OrderBy(b => b.PriceFirstHour).ToListAsync();
+                case "PriceOfAdditionalHours": 
+                    return await _context.Bikes
+                        .Where(b => _context.Rentals.Find(b.BikeId) == null ||
+                                _context.Rentals.Find(b.BikeId).Paid ||
+                                _context.Rentals.Find(b.BikeId).TotalCost == 0)
+                        .OrderBy(b => b.PriceAdditionalHours).ToListAsync();
+                case "PurchaseDate": 
+                    return await _context.Bikes
+                        .Where(b => _context.Rentals.Find(b.BikeId) == null ||
+                                _context.Rentals.Find(b.BikeId).Paid ||
+                                _context.Rentals.Find(b.BikeId).TotalCost == 0)
+                        .OrderByDescending(b => b.PurchesDate).ToListAsync();
+                default:
+                    return await _context.Bikes
+                        .Where(b => _context.Rentals.Find(b.BikeId) == null ||
+                                _context.Rentals.Find(b.BikeId).Paid ||
+                                _context.Rentals.Find(b.BikeId).TotalCost == 0).ToListAsync();
+            }
+                
+            
+        }
+
+        // GET: api/Bikes/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Bike>> GetBike(int id)
+        {
+            var bike = await _context.Bikes.FindAsync(id);
+
+            if (bike == null)
+            {
+                return NotFound();
+            }
+
+            return bike;
+        }
+
+        // PUT: api/Bikes/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutBike(int id, Bike bike)
+        {
+            if (id != bike.BikeId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(bike).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!BikeExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Bikes
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPost]
+        public async Task<ActionResult<Bike>> PostBike(Bike bike)
+        {
+            _context.Bikes.Add(bike);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetBike", new { id = bike.BikeId }, bike);
+        }
+
+        // DELETE: api/Bikes/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Bike>> DeleteBike(int id)
+        {
+            var bike = await _context.Bikes.FindAsync(id);
+            if (bike == null)
+            {
+                return NotFound();
+            }
+
+            _context.Bikes.Remove(bike);
+            await _context.SaveChangesAsync();
+
+            return bike;
+        }
+
+        private bool BikeExists(int id)
+        {
+            return _context.Bikes.Any(e => e.BikeId == id);
+        }
+    }
+}
---
+++ BikeRental/Controllers/CustomersController.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using BikeRental;
+using BikeRental.Model;
+
+namespace BikeRental.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomersController : ControllerBase
+    {
+        private readonly BikeRentalContext _context;
+
+        public CustomersController(BikeRentalContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Customers
+        [HttpGet]
+        [Route("{lastNameFilter?}")]
+        public async Task<ActionResult<IEnumerable<Customer>>> GetCustomers(string lastNameFilter = null)
+        {
+            if(lastNameFilter != null)
+            {
+                return await _context.Customers.Where(c => c.LastName == lastNameFilter).ToListAsync();
+            }
+            return await _context.Customers.ToListAsync();
+        }
+
+        // GET: api/Customers/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Customer>> GetCustomer(int id)
+        {
+            var customer = await _context.Customers.FindAsync(id);
+
+            if (customer == null)
+            {
+                return NotFound();
+            }
+
+            return customer;
+        }
+
+        // PUT: api/Customers/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCustomer(int id, Customer customer)
+        {
+            if (id != customer.CustomerId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(customer).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CustomerExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Customers
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPost]
+        public async Task<ActionResult<Customer>> PostCustomer(Customer customer)
+        {
+            _context.Customers.Add(customer);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetCustomer", new { id = customer.CustomerId }, customer);
+        }
+
+        // DELETE: api/Customers/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Customer>> DeleteCustomer(int id)
+        {
+            var customer = await _context.Customers.FindAsync(id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+
+            _context.Customers.Remove(customer);
+            await _context.SaveChangesAsync();
+
+            return customer;
+        }
+        // GET: api/Customers/5/rentals
+        [HttpGet("{id}/rentals")] // Should work probably, haven't tested it yet
+        public async Task<ActionResult<IEnumerable<Rental>>> GetCustomerRentals(int id)
+        {
+            var customer = await _context.Customers.FindAsync(id);
+            return customer.Rentals.ToList();
+        }
+
+        private bool CustomerExists(int id)
+        {
+            return _context.Customers.Any(e => e.CustomerId == id);
+        }
+    }
+}
